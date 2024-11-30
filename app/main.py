f2b805ffd3b2d0f8a8b2c3bea2d8387d5a1ed0be
aurelblankfactor <aurel.chivu@blankfactor.com>
2024-11-30 19:39:31 +0200

    Added root router

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
+from collections.abc import AsyncGenerator
+from typing import AsyncContextManager
+
 from fastapi import FastAPI
 
-from app.core.database import Base, engine, get_db
+from app.core.database import Base, engine
+from app.api.v1.routers.root import router as root_router
 from app.api.v1.routers.group import router as group_router
 from app.api.v1.routers.user import router as user_router
 from app.utils.database_healt_check import check_db_health
@@ -9,7 +13,7 @@ from app.utils.custom_logger import logger
 Base.metadata.create_all(bind=engine)
 
 
-async def lifespan(app):
+async def lifespan(app: FastAPI) -> AsyncContextManager:
     if not check_db_health():
         logger.error("Database connection failed during startup")
         raise RuntimeError("Database connection failed during startup")
@@ -19,14 +23,6 @@ async def lifespan(app):
 
 app = FastAPI(lifespan=lifespan)
 
+app.include_router(root_router)
 app.include_router(user_router)
 app.include_router(group_router)
-
-
-@app.get("/")
-def root():
-    return {
-        "message": "Welcome to the user and group management API",
-        "docs": "/docs",
-        "redoc": "/redoc",
-    }
